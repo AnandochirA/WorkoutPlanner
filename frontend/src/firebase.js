7d73c98ee9fe39627fd0c91ab2c7bb40af4ee2da
AnandochirA <amartua295@lynchburg.edu>
2024-08-22 17:17:50 -0400

    Work of Firstday

---
+++ frontend/src/firebase.js
@@ -0,0 +1,24 @@
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// Import other Firebase services you plan to use, e.g., Firestore, Auth
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBVlhuxgFkNt0CgVYVdIPPUIR0Nv9b3n4g",
+  authDomain: "workout-planner-939b2.firebaseapp.com",
+  projectId: "workout-planner-939b2",
+  storageBucket: "workout-planner-939b2.appspot.com",
+  messagingSenderId: "772979454731",
+  appId: "1:772979454731:web:42e0b5504e2fa3301da80e",
+  measurementId: "G-Z46LR9BZ58"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const auth = getAuth(app); // Firebase Auth
+const db = getFirestore(app); // Firestore Database
+
+export { auth, db, analytics };
