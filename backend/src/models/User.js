65e216aca50a8f08fa61e2cc3aa47e9ad80ad564
AnandochirA <158313510+AnandochirA@users.noreply.github.com>
2024-09-15 23:42:12 -0400

    Update User.js

--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -1,7 +1,8 @@
-export interface User {
-  id: string;
-  name: string;
-  email: string;
-  createdAt: Date;
-  workoutPlans: string[]; //workout plan IDs
-}
+/**
+ * @typedef {Object} User
+ * @property {string} id - The unique identifier for the user.
+ * @property {string} name - The name of the user.
+ * @property {string} email - The email address of the user.
+ * @property {Date} createdAt - The date when the user was created.
+ * @property {string[]} workoutPlans - An array of workout plan IDs associated with the user.
+ */
