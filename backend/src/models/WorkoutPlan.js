e41c0ba3a3496df189f60db752c3a5aee5073027
AnandochirA <158313510+AnandochirA@users.noreply.github.com>
2024-09-15 23:42:27 -0400

    Update WorkoutPlan.js

--- backend/src/models/WorkoutPlan.js
+++ backend/src/models/WorkoutPlan.js
@@ -1,11 +1,10 @@
-export interface WorkoutPlan {
-  id: string;
-  userId: string;
-  title: string;
-  description?: string; //Optional
-  days: {
-    [day: string]: string[]; //Exercises by day, array of Exercise IDs
-  };
-  createdAt: Date;
-  updatedAt: Date;
-}
+/**
+ * @typedef {Object} WorkoutPlan
+ * @property {string} id - The unique identifier for the workout plan.
+ * @property {string} userId - The unique identifier for the user associated with the workout plan.
+ * @property {string} title - The title of the workout plan.
+ * @property {string} [description] - A description of the workout plan (optional).
+ * @property {Object.<string, string[]>} days - Exercises by day, where the key is the day (e.g., 'Monday') and the value is an array of Exercise IDs.
+ * @property {Date} createdAt - The date when the workout plan was created.
+ * @property {Date} updatedAt - The date when the workout plan was last updated.
+ */
